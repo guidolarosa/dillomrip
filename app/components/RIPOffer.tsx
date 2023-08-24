3292257768e5def46fef349ef4a7491ee6c0c88e
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-24 18:36:42 -0300

    Updated styles

--- app/components/RIPOffer.tsx
+++ app/components/RIPOffer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const RIPOffer = ( props : any ) => {
   return (
     <div className={`flex flex-col items-center absolute bottom-0 ${props.position} animate-fadeup opacity-0`} style={{animationDelay: `${((props.index + 3) * 750)}ms`}}>
-      <div className={`relative w-[115px] h-[115px]`}>
+      <div className={`relative w-[92px] h-[92px]`}>
         <Image src={props.image} fill alt={props.name} /> 
       </div>
       <div className="whitespace-nowrap text-center text-[22px]">
