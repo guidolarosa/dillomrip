7ff784f93b83394b1fc4710170fdcf0726f0739c
Guido La Rosa <guidolarosa@MacBook-Pro-de-Guido.local>
2023-08-23 22:08:06 -0300

    Updated styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="opening fixed w-[100vw] h-[100dvh] z-10">
+        <Image src="/opening.gif" fill alt="Opening" />
+      </div>
+      <div className="background absolute w-[100vw] h-[100dvh] z-0">
+        <Image src="/bkg.jpg" fill alt="Opening" className="object-cover object-top"/>
+      </div>
+
+      <div>
+
+      </div>
     </main>
   )
 }
