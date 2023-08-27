9fce92e49720fa90eaa931d73293d08811a1487a
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-27 16:34:19 -0300

    Updated styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,17 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/offers/ofrenda-1.png" sizes="any" />
         <meta name="robots" content="nofollow, noindex" />
+        <meta
+          name="image"
+          content="https://www.dillom.rip/wp-content/uploads/2023/08/share.jpg"
+        />
+        <meta property="title" content="Home - Dillom" />
+        <meta
+          property="description"
+          content="@odd mami @Muerejoven @k4 @lil quentin @Broke Carrey @Saramalacara La vida guió tu camino, La muerte tu destino, Solemne es nuestro rezo ante tu partida, Que tu alma en paz descanse, Esta es nuestra despedida. : 00 : 00 : 00"
+        />
+        <meta property="url" content="https://www.dillom.rip/" />
+
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Home - Dillom" />
