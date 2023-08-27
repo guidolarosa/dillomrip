f758d931929e06c412cfaeacd189e7b72bc1455f
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-27 11:09:36 -0300

    Updated styles

--- app/components/OfferSubmitForm.tsx
+++ app/components/OfferSubmitForm.tsx
@@ -54,7 +54,7 @@ const OfferSubmitForm = (props: any) => {
         <input
           value={props.offerText}
           placeholder="Dejá tu ofrenda..."
-          className={`mt-20 bg-transparent border-b text-center text-[32px] mb-8 outline-none transition-all placeholder:opacity-50`}
+          className={`mt-20 bg-transparent border-b text-center text-[32px] mb-8 outline-none transition-all placeholder:opacity-50 rounded-none`}
           onChange={onOfferUpdate}
         />
         <div
