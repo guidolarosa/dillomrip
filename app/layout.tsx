de11dd38ecd02a779e5da674dfdba47973612235
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-28 19:20:59 -0300

    Updated tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/offers/ofrenda-1.png" sizes="any" />
         {/* Google Tag Manager */}
-        <Script>
+        <Script id="gtag">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
