a1ccf06eaeab21f15a3a742d9397c5f5eb5be785
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-28 18:53:11 -0300

    Updated rip offers

--- app/components/RIPOffer.tsx
+++ app/components/RIPOffer.tsx
@@ -6,8 +6,14 @@ const RIPOffer = (props: any) => {
       className={`flex flex-col items-center absolute translate-y-[-40px] lg:translate-y-0 lg:bottom-0 animate-fade opacity-0`}
       style={{
         animationDelay: `${(props.index + 8) * 750}ms`,
-        bottom: `${props.position.bottom}%`,
-        left: `${props.position.left}%`
+        bottom:
+          window.innerWidth >= 768
+            ? `${props.position.desktop.bottom}%`
+            : `${props.position.mobile.bottom}%`,
+        left:
+          window.innerWidth >= 768
+            ? `${props.position.desktop.left}%`
+            : `${props.position.mobile.left}%`,
       }}
     >
       <div className={`relative aspect-square w-[80px] lg:w-[120px]`}>
