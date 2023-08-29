fdc5a760d737bba40d921cd9dcad8ba7eb098202
Guido La Rosa <guido.glarosa@gmail.com>
2023-08-29 12:34:40 -0300

    Updated twitter link

--- app/components/Share.tsx
+++ app/components/Share.tsx
@@ -16,7 +16,7 @@ const Share = (props: any) => {
         offerImage={props.offerImage}
       />
       <div className="w-[320px] aspect-square relative top-4 flex flex-col justify-center items-center">
-        <Link href="https://twitter.com/intent/tweet?&url=https%3A%2F%2Fwww.dillom.rip%2F&text=Que%20tu%20alma%20en%20paz%20descanse,%20esta%20es%20nuestra%20despedida" target="_blank">
+        <Link href="https://twitter.com/intent/tweet?&url=https%3A%2F%2Fdillom.rip%2F&text=Que%20tu%20alma%20en%20paz%20descanse,%20esta%20es%20nuestra%20despedida" target="_blank">
           <div
             className={`w-[180px] h-[62px] md:w-[331px] md:h-[80px] relative transition duration-500 drop-shadow-[0px_0px_8px_transparent]  hover:drop-shadow-[0_0_8px_white]`}
           >
@@ -29,8 +29,7 @@ const Share = (props: any) => {
           </div>
         </Link>
         <div className="flex justify-center mt-2">
-          {/* <Link href="https://twitter.com/intent/tweet?&url=https%3A%2F%2Fwww.dillom.rip%2F&text=Que%20tu%20alma%20en%20paz%20descanse,%20esta%20es%20nuestra%20despedida&via=sometwitterusername&hashtags=hashtag1%2Chashtag2" target="_blank"> */}
-          <Link href="https://twitter.com/intent/tweet?&url=https%3A%2F%2Fwww.dillom.rip%2F&text=Que%20tu%20alma%20en%20paz%20descanse,%20esta%20es%20nuestra%20despedida" target="_blank">
+          <Link href="https://twitter.com/intent/tweet?&url=https%3A%2F%2Fdillom.rip%2F&text=Que%20tu%20alma%20en%20paz%20descanse,%20esta%20es%20nuestra%20despedida" target="_blank">
             <FaTwitter className="mx-2" size="32px"/>
           </Link>
           <Link download href="/share.jpeg" className="block md:hidden">
