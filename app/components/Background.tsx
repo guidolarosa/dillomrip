0b4c4552e750264530171bfc11842b90b04f1193
Guido La Rosa <guido.glarosa@gmail.com>
2023-09-01 19:00:00 -0300

    Uploaded uptades

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,7 +1,7 @@
 const Background = () => {
   return (
     <div
-      className="absolute w-[100vw] h-full z-0 bg-[url(/fondo_large_mobile.png)] sm:bg-[url(/bg-large.png)] bg-[-15rem_0rem] sm:bg-[center_top_-12rem] sm:bg-[length:100%_auto] bg-no-repeat"
+      className="absolute w-[100vw] h-full z-0 bg-[url(/fondo_large_mobile.png)] sm:bg-[url(/bg-large.png)] bg-[0rem_-10rem] sm:bg-[center_top_-12rem] bg-[length:100%_auto] sm:bg-[length:100%_auto] bg-no-repeat"
     />
   );
 };
