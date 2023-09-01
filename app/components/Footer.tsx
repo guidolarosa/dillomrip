7d0ab27dfa2c8e7d17d823aca88ea1c24ff5081f
Guido La Rosa <guido.glarosa@gmail.com>
2023-09-01 12:27:33 -0300

    Added animated footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,53 +1,75 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Countdown from "./Countdown";
 import ReleaseDate from "./ReleaseDate";
 
 const Footer = (props: any) => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    if (props.fixed) {
+      const onScroll = () => {
+        console.log(window.scrollY)
+        if (window.scrollY > 500) {
+          setShowFooter(true);
+        } else {
+          setShowFooter(false);
+        }
+      };
+
+      window.addEventListener("scroll", onScroll);
+
+      return () => {
+        window.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, []);
+
   return (
-  <>
-    <footer className="flex justify-center bg-[url('/fire-footer.png')] w-full h-[280px] md:h-[700px] pb-4 md:pb-[32px] overflow-hidden relative flex-col items-center mt-auto z-20">
-      <div className="absolute w-full h-full z-10 bottom-10 md:bottom-0">
-        <Image
-          fill
-          src="/fire-footer.png"
-          alt=""
-          className="object-top object-cover"
-        />
-      </div>
-      <div className="bottom-10 lg:bottom-0 absolute w-full h-full">
-        <Image
-          fill
-          src="/branch-footer.png"
-          alt=""
-          className="object-top object-cover"
-        />
-      </div>
-      <div className="relative flex flex-col z-50 mt-auto">
-        {props.countdown ? (
-          <Countdown />
-        ) : (
-          <ReleaseDate />
-        )}
-      </div>
-      <div className="relative w-[96px] h-[60px] z-50 drop-shadow-[0px_0px_8px_transparent] hover:drop-shadow-[0px_0px_8px_white] cursor-pointer">
-        <Link href="https://discord.gg/ZWjqRD6x" target="_blank">
+    <>
+      <footer
+        className={`flex justify-center bg-[url('/fire-footer.png')] w-full h-[280px] md:h-[700px] pb-4 md:pb-[32px] overflow-hidden  flex-col items-center mt-auto z-20 transition-all duration-[2s] ${
+          props.fixed ? (showFooter ? "fixed bottom-0" : "fixed bottom-[-300px] md:bottom-[-800px]") : "relative"
+        }`}
+      >
+        <div className={`absolute w-full h-full z-10 ${props.fixed ? 'bottom-0' : 'bottom-10 md:bottom-0'}`}>
           <Image
             fill
-            src="/contacto.svg"
+            src="/fire-footer.png"
             alt=""
-            className="object-top object-contain"
+            className="object-top object-cover"
           />
-        </Link>
-      </div>
-    </footer>
-    <div className="gradient fixed bottom-0 bg-gradient-footer-mask h-40 w-[100vw] z-10" />
-  </>
+        </div>
+        <div className={`bottom-10 lg:bottom-0 absolute w-full h-full ${props.fixed ? 'bottom-0' : 'bottom-10 md:bottom-0'}`}>
+          <Image
+            fill
+            src="/branch-footer.png"
+            alt=""
+            className="object-top object-cover"
+          />
+        </div>
+        <div className="relative flex flex-col z-50 mt-auto">
+          {props.countdown ? <Countdown /> : <ReleaseDate />}
+        </div>
+        <div className="relative w-[96px] h-[60px] z-50 drop-shadow-[0px_0px_8px_transparent] hover:drop-shadow-[0px_0px_8px_white] cursor-pointer">
+          <Link href="https://discord.gg/ZWjqRD6x" target="_blank">
+            <Image
+              fill
+              src="/contacto.svg"
+              alt=""
+              className="object-top object-contain"
+            />
+          </Link>
+        </div>
+      </footer>
+      <div className="gradient fixed bottom-0 bg-gradient-footer-mask h-40 w-[100vw] z-10" />
+    </>
   );
 };
 
 Footer.defaultProps = {
-  countdown: true
-}
+  countdown: true,
+};
 
 export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -45,6 +45,7 @@ export default function Home() {
         )}
       </main>
       <Footer 
+        fixed
         countdown={false}
       />
     </>
